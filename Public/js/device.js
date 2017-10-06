cdfe12063b8c6cd946ffcc38d4e8b0352a221709
Rishabh Tayal <rtayal11@gmail.com>
2017-10-06 11:35:14 +0530

    added clap icon

--- Public/js/device.js
+++ Public/js/device.js
@@ -2,7 +2,8 @@ function clapClicked(device_id) {
 	console.log('clap click');
 	var button = document.getElementById('clap-button-'+ device_id);
 	$.post('/clap', { device_id: device_id}).done(function(result) {
-		button.innerHTML = result["claps_count"];
+		// button.innerHTML = result["claps_count"];
+		button.value = result["claps_count"];
 	}).fail(function() {
 
 	});
