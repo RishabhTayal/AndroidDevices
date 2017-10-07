58e43f8300bfbe5874def9529f234dcc0af83639
Rishabh Tayal <rtayal11@gmail.com>
2017-10-07 07:44:25 +0530

    seed

--- Scripts/seed_manufacturers.sql
+++ Scripts/seed_manufacturers.sql
@@ -1,5 +1,6 @@
 INSERT INTO "public"."manufacturer"("id","name","website_link","approved")
 VALUES
-(4,E'Google',E'google.com',TRUE),
 (5,E'Samsung',E'samsung.com',TRUE),
-(6,E'HTC',E'htc.com',TRUE);
+(6,E'HTC',E'htc.com',TRUE),
+(4,E'Google',E'google.com',TRUE),
+(7,E'OnePlus',E'https://oneplus.net',TRUE);
